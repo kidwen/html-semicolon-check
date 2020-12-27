feecc99ca0d8512fd7569a252194ea4ac54532c2
kidwen <2061895077@qq.com>
2020-12-27 22:38:12 +0800

    fix eslint

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,31 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+/**
+ * this method is called when your extension is activated
+ * your extension is activated the very first time the command is executed
+ * @param {vscode.ExtensionContext} context
+ * @return {void}
+ */
 export function activate(context: vscode.ExtensionContext) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "html-semicolon-check" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "html-semicolon-check" is now active!');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    const disposable = vscode.commands.registerCommand('html-semicolon-check.helloWorld', () => {
+        // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('html-semicolon-check.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Hello World from Html Semicolon Check!');
+    });
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Html Semicolon Check!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+/**
+ * this method is called when your extension is deactivated
+ */
+export function deactivate() { }
