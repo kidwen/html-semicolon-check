a31c6d2e168b2739325441aea876c6cb092aba9d
kidwen <2061895077@qq.com>
2021-01-10 23:41:09 +0800

    add syntax config for html  file

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    const disposable = vscode.languages.registerHoverProvider('html', {
+    const disposable = vscode.languages.registerHoverProvider({ scheme: 'file', language: 'html' }, {
         // The code you place here will be executed every time your command is executed
         provideHover(document, position, token) {
             return {
